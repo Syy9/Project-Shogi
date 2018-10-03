0b139faea3c627f37e185cd1938c3cde66c1b00e
Syy <shirakawa.yy@gmail.com>
2018-10-03 22:22:17 +0900

    Add KomaIconLoader

---
+++ Assets/Scripts/Loader/KomaIconLoader.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class KomaIconLoader
+{
+    private static readonly string KOMA_PATH = "Koma/";
+    public Sprite Load(string fileName)
+    {
+        return Resources.Load<Sprite>(KOMA_PATH + fileName);
+    }
+}
