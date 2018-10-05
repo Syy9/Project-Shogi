15269a35f9d33552593da8e88f97afbe2a3f495e
Syy <shirakawa.yy@gmail.com>
2018-10-05 23:44:35 +0900

    Add UIBoardGridManager

---
+++ Assets/Scripts/UI/UIBoardGridManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIBoardGridManager : MonoBehaviour {
+    public const int COLUMN_COUNT = 9;
+    public const int ROW_COUNT = 9;
+    public Vector2 GetPosition(int x, int y)
+    {
+        int index = y * COLUMN_COUNT + x;
+        var grid = transform.GetChild(index);
+        return grid.transform.position;
+    }
+}
