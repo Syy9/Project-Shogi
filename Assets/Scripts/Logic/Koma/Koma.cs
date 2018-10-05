a40c02aef7218a675f089b8832a3e8b55d6319b0
Syy <shirakawa.yy@gmail.com>
2018-10-05 23:57:14 +0900

    Add Koma

---
+++ Assets/Scripts/Logic/Koma/Koma.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+
+public class Koma : MonoBehaviour {
+    [Inject] UIBoardGridManager UIBoardGridManager;
+
+    IEnumerator Start()
+    {
+        Debug.Log("hogehogeStart");
+        for (int x = 0; x < 9; x++)
+        {
+            for (int y = 0; y < 9; y++)
+            {
+                yield return new WaitForSeconds(1);
+                Move(x, y);
+                Debug.Log("x " + x  +"y" + y);
+            }
+        }
+    }
+    public void Move(int x, int y)
+    {
+        var position = UIBoardGridManager.GetPosition(x, y);
+        transform.position = position;
+    }
+}
