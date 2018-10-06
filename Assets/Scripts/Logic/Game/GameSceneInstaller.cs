9f4e353f06eddfb0e2d484a5c546828df5e9c8b1
Syy <shirakawa.yy@gmail.com>
2018-10-06 21:15:42 +0900

    Bind

--- Assets/Scripts/Logic/Game/GameSceneInstaller.cs
+++ Assets/Scripts/Logic/Game/GameSceneInstaller.cs
@@ -4,8 +4,12 @@ using Zenject;
 public class GameSceneInstaller : MonoInstaller<GameSceneInstaller>
 {
     [SerializeField] UIBoardGridManager GridManager;
+    [SerializeField] UIBoard UIBoard;
     public override void InstallBindings()
     {
+        Container.Bind<UIBoard>().FromInstance(UIBoard);
         Container.Bind<UIBoardGridManager>().FromInstance(GridManager);
+        Container.Bind<Koma>().FromComponentInNewPrefabResource("Prefabs/Koma").AsSingle();
+        Container.Bind<IKomaGenerator>().To<KomaGenerator>().AsSingle();
     }
 }
