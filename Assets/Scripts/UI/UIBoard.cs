a6ed1045553b2f20d60e167d946397ec2e0b61ee
Syy <shirakawa.yy@gmail.com>
2018-10-06 21:14:54 +0900

    Add UIBoard class

---
+++ Assets/Scripts/UI/UIBoard.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIBoard : MonoBehaviour {
+    [SerializeField] Transform komaParent;
+
+    public Transform GetKomaParent()
+    {
+        return komaParent;
+    }
+}
