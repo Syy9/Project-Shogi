f2e18bf2936a85de439c3e0b70bb494daeac05d7
Syy <shirakawa.yy@gmail.com>
2018-10-06 21:15:11 +0900

    Add GameSceneController

---
+++ Assets/Scripts/Logic/Game/GameSceneController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+
+public class GameSceneController : MonoBehaviour
+{
+    [Inject] IKomaGenerator komaGenerator;
+	IEnumerator Start () {
+		for (int i = 0; i < 10; i++)
+        {
+            yield return new WaitForSeconds(1);
+            komaGenerator.Create();
+        }
+	}
+}
