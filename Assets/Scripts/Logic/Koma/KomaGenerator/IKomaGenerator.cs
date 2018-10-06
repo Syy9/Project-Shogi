ea349caa76e674bbbc3459b362226e8a5feade06
Syy <shirakawa.yy@gmail.com>
2018-10-06 21:15:03 +0900

    Add KomaGenerator

---
+++ Assets/Scripts/Logic/Koma/KomaGenerator/IKomaGenerator.cs
@@ -0,0 +1,4 @@
+﻿public interface IKomaGenerator
+{
+    Koma Create();
+}
---
+++ Assets/Scripts/Logic/Koma/KomaGenerator/KomaGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+
+public class KomaGenerator : IKomaGenerator
+{
+    Koma _komaPrefab;
+    UIBoard _uiBoard;
+
+    public KomaGenerator(Koma komaPrefab, UIBoard uiBoard)
+    {
+        _komaPrefab = komaPrefab;
+        _uiBoard = uiBoard;
+    }
+
+    public Koma Create()
+    {
+        var parent = _uiBoard.GetKomaParent();
+        var koma = GameObject.Instantiate<Koma>(_komaPrefab, parent);
+        return koma;
+    }
+}
