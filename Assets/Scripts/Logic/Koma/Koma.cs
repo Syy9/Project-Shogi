70c5d00729ba83c157dabaf69d1d8a1cb6d9f69e
Syy <shirakawa.yy@gmail.com>
2018-10-08 16:55:44 +0900

    Add DisallowMultipleComponent attribute

--- Assets/Scripts/Logic/Koma/Koma.cs
+++ Assets/Scripts/Logic/Koma/Koma.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Zenject;
 
+[DisallowMultipleComponent]
 public class Koma : MonoBehaviour {
     [Inject] UIBoardGridManager UIBoardGridManager;
 
