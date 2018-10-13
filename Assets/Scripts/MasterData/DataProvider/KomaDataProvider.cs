2dcf798a55b6d7645aad5f8871e7ccf07ce336d7
Syy <shirakawa.yy@gmail.com>
2018-10-13 22:55:49 +0900

    Add KomaDataProvider

---
+++ Assets/Scripts/MasterData/DataProvider/KomaDataProvider.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+namespace MasterData
+{
+    public interface IKomaDataProvider
+    {
+        IEnumerable<KomaData> Data { get; }
+    }
+    public class KomaDataProvider : IDataCreater<KomaData> , IKomaDataProvider
+    {
+        public IEnumerable<KomaData> Data { get; private set; }
+
+        public KomaDataProvider()
+        {
+            Data = Create();
+        }
+
+        public IEnumerable<KomaData> Create()
+        {
+            var data = new List<KomaData>();
+            data.Add(new KomaData() { KomaType = KomaType.Type001, IconAssetName = "sgl01" });
+            data.Add(new KomaData() { KomaType = KomaType.Type002, IconAssetName = "sgl02" });
+            data.Add(new KomaData() { KomaType = KomaType.Type003, IconAssetName = "sgl03" });
+            data.Add(new KomaData() { KomaType = KomaType.Type004, IconAssetName = "sgl04" });
+            data.Add(new KomaData() { KomaType = KomaType.Type005, IconAssetName = "sgl05" });
+            return data;
+        }
+    }
+}
