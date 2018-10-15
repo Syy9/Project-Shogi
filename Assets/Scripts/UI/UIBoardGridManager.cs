3334af7e52486324969649c4f73c4c8779d2e4f9
Syy <shirakawa.yy@gmail.com>
2018-10-16 00:27:37 +0900

    Add func to OnSelect event

--- Assets/Scripts/UI/UIBoardGridManager.cs
+++ Assets/Scripts/UI/UIBoardGridManager.cs
@@ -1,10 +1,27 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIBoardGridManager : MonoBehaviour {
     public const int COLUMN_COUNT = 9;
     public const int ROW_COUNT = 9;
+
+    public event Action<Vector2Int> OnSelect;
+
+    void Awake()
+    {
+        var buttons = GetComponentsInChildren<Button>();
+        for (int i = 0; i < buttons.Length; i++)
+        {
+            int x = i % COLUMN_COUNT;
+            int y = i / ROW_COUNT;
+            buttons[i].onClick.AddListener(() => {
+                OnSelect.Call(new Vector2Int(x, y));
+            });
+        }
+    }
     public Vector2 GetPosition(int x, int y)
     {
         int index = y * COLUMN_COUNT + x;
