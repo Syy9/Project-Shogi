2380256b8d579fbb15007feeaaf0b0bf3399aa14
Syy <shirakawa.yy@gmail.com>
2018-10-16 00:09:18 +0900

    Add CoroutineService

---
+++ Assets/Scripts/Framework/CoroutineService.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CoroutineService
+{
+    private static CoroutineUpdater Updater;
+    public void Start(IEnumerator coroutine)
+    {
+        if(Updater == null)
+        {
+            var obj = new GameObject("CoroutineUpdater");
+            GameObject.DontDestroyOnLoad(obj);
+            Updater = obj.AddComponent<CoroutineUpdater>();
+            
+        }
+        Updater.Push(coroutine);
+    }
+
+    public class CoroutineUpdater : MonoBehaviour
+    {
+        public void Push(IEnumerator coroutine)
+        {
+            StartCoroutine(coroutine);
+        }
+    }
+}
