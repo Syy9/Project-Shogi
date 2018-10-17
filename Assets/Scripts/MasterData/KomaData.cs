de38cf58202ed14a0f7d2da3c3e4030259641bf2
Syy <shirakawa.yy@gmail.com>
2018-10-17 23:32:59 +0900

    Add comment

--- Assets/Scripts/MasterData/KomaData.cs
+++ Assets/Scripts/MasterData/KomaData.cs
@@ -13,7 +13,10 @@
         Type001, //王
         Type002, //飛車
         Type003, //角
-        Type004,
-        Type005,
+        Type004, //金
+        Type005, //銀
+        Type006, //桂馬
+        Type007, //香車
+        Type008, //歩兵
     }
 }
