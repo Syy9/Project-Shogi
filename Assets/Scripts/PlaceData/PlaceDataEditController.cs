8183c9c6a7e6210946a31d1757239892a71bbb6e
Syy <shirakawa.yy@gmail.com>
2018-10-23 23:21:51 +0900

    Add namespace

--- Assets/Scripts/PlaceData/PlaceDataEditController.cs
+++ Assets/Scripts/PlaceData/PlaceDataEditController.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using MasterData;
 using UnityEngine;
 using Zenject;
+namespace PlaceData.Edit
+{
+    public class PlaceDataEditController : MonoBehaviour
+    {
+        [Inject] public CoroutineService CoroutineService { get; private set; }
+        [Inject] public IFixedDataManager FixedDataManager { get; private set; }
+        [Inject] public IKomaFactory KomaFactory { get; private set; }
+        [Inject] public IUIBoard UIBoard { get; private set; }
 
-public class PlaceDataEditController : MonoBehaviour {
-    [Inject] public CoroutineService CoroutineService { get; private set; }
-    [Inject] public IFixedDataManager FixedDataManager { get; private set; }
-    [Inject] public IKomaFactory KomaFactory { get; private set; }
-    [Inject] public IUIBoard UIBoard { get; private set; }
+    }
 }
--- Assets/Scripts/PlaceData/PlaceDataEditInstaller.cs
+++ Assets/Scripts/PlaceData/PlaceDataEditInstaller.cs
@@ -1,25 +1,28 @@
 using MasterData;
 using UnityEngine;
 using Zenject;
-
-public class PlaceDataEditInstaller : MonoInstaller<PlaceDataEditInstaller>
+namespace PlaceData.Edit
 {
-    [SerializeField] UIBoard UIBoardPrefab;
-    [SerializeField] Transform UIBoardParent;
-    public override void InstallBindings()
+    public class PlaceDataEditInstaller : MonoInstaller<PlaceDataEditInstaller>
     {
-        //Framework
-        Container.Bind<CoroutineService>().ToSelf().AsSingle();
+        [SerializeField] UIBoard UIBoardPrefab;
+        [SerializeField] Transform UIBoardParent;
+        public override void InstallBindings()
+        {
+            //Framework
+            Container.Bind<CoroutineService>().ToSelf().AsSingle();
 
-        //System
-        Container.Bind<IFixedDataManager>().To<FixedDataManager>().AsSingle();
+            //System
+            Container.Bind<IFixedDataManager>().To<FixedDataManager>().AsSingle();
 
-        //Game
-        Container.Bind<IUIBoard>().FromComponentInNewPrefab(UIBoardPrefab).UnderTransform(UIBoardParent).AsSingle().NonLazy();
-        Container.Bind<KomaIconLoader>().ToSelf().AsSingle();
-        Container.BindFactoryCustomInterface<Koma.InitData, Koma, Koma.Factory, IKomaFactory>().FromFactory<KomaFactory>();
+            //Game
+            Container.Bind<IUIBoard>().FromComponentInNewPrefab(UIBoardPrefab).UnderTransform(UIBoardParent).AsSingle().NonLazy();
+            Container.Bind<KomaIconLoader>().ToSelf().AsSingle();
+            Container.BindFactoryCustomInterface<Koma.InitData, Koma, Koma.Factory, IKomaFactory>().FromFactory<KomaFactory>();
 
-        //State
-        Container.Bind<GameStateManager>().ToSelf().AsSingle();
+            //State
+            Container.Bind<GameStateManager>().ToSelf().AsSingle();
+        }
     }
+
 }
