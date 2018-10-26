1c7e4b4e16a3a15c20b3fb63b549d1bc92b964c8
Syy <shirakawa.yy@gmail.com>
2018-10-26 23:52:34 +0900

    Add PlaceDataEditSlotController

---
+++ Assets/Scripts/PlaceData/PlaceDataEditSlotController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+namespace PlaceData.Edit
+{
+    public class PlaceDataEditSlotController : MonoBehaviour
+    {
+        public Action<Koma> OnSelect { private get; set; }
+        List<Slot> _slotList = new List<Slot>();
+        public void Add(Koma koma)
+        {
+            var obj = new GameObject();
+            obj.transform.SetParent(transform);
+            var slot = obj.AddComponent<Slot>();
+            slot.OnSelect = OnSelectEvent;
+            slot.Add(koma);
+        }
+
+        public void RemoveAll()
+        {
+            foreach (var slot in _slotList)
+            {
+                GameObject.Destroy(slot.gameObject);
+            }
+            _slotList.Clear();
+        }
+
+        public void Remove(Koma koma)
+        {
+            var slot = _slotList.FirstOrDefault(data => data.Koma == koma);
+            if(slot == null)
+                return;
+
+            slot.OnSelect = null;
+            _slotList.Remove(slot);
+            GameObject.Destroy(slot);
+        }
+
+        void OnSelectEvent(Koma koma)
+        {
+            OnSelect.Call(koma);
+        }
+    }
+
+    public class Slot : MonoBehaviour
+    {
+        public Koma Koma { get; private set; }
+        public Action<Koma> OnSelect { private get; set; }
+
+        void Awake()
+        {
+            gameObject.AddComponent<Image>();
+            var button = gameObject.AddComponent<Button>();
+            button.onClick.AddListener(() => {
+                OnSelect.Call(Koma);
+            });
+        }
+
+        public void Add(Koma koma)
+        {
+            Koma = koma;
+            koma.transform.SetParent(transform);
+        }
+    }
+}
