982ebad137c6311e4d0e44c5dfbf03476b23058a
Syy <shirakawa.yy@gmail.com>
2018-11-08 22:23:05 +0900

    Add func to koma move select position

--- Assets/Scripts/Logic/Game/GameState/State/GameStateSelect.cs
+++ Assets/Scripts/Logic/Game/GameState/State/GameStateSelect.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 
 public class GameStateSelect : GameStateBase
 {
+    Vector2Int NoSelectPosition = new Vector2Int(-1, -1);
+    Vector2Int _selectPosition;
     protected override void OnEnter()
     {
+        _selectPosition = NoSelectPosition;
         Owner.UIBoard.OnSelect += OnSelect;
     }
 
@@ -17,9 +20,33 @@ public class GameStateSelect : GameStateBase
 
     void OnSelect(Vector2Int position)
     {
-        var initData = new Koma.InitData();
-        initData.Type = KomaType.Type001;
-        initData.InitPosition = position;
-        Owner.KomaFactory.Create(initData);
+        if (_selectPosition == position)
+        {
+            Owner.UIBoard.SetHighlight(position, false);
+            return;
+        }
+
+        if (_selectPosition != NoSelectPosition) //移動先選択中
+        {
+            if (Owner.KomaManager.IsEmpty(position))
+            {
+                var koma = Owner.KomaManager.Find(_selectPosition);
+                koma.Move(position);
+            }
+            Owner.UIBoard.SetHighlight(_selectPosition, false);
+            _selectPosition = NoSelectPosition;
+        }
+        else
+        {
+            if (Owner.KomaManager.IsNotEmpty(position))
+            {
+                Owner.UIBoard.SetHighlight(position, true);
+                _selectPosition = position;
+            }
+            else
+            {
+                _selectPosition = NoSelectPosition;
+            }
+        }
     }
 }
